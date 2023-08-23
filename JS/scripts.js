b23a8ebd55f6eb03896a23dbc4d23fa8b8939646
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-08-23 18:47:41 +0200

    Initial commit

---
+++ JS/scripts.js
@@ -0,0 +1,10 @@
+
+
+alert("Hello World!!!");
+
+let myName="Gitashree Mahato  ";
+document.write(myName);
+
+
+var favoriteFood= ["Egg Biriyani", "Noodles", "Donner"];
+document.write("My favorite foods are: " + favoriteFood);
