d9e2207cc5181376777b9f7c61ee22b781193dae
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-08-24 23:08:11 +0200

    update index.html
    Commented code of Ex 1.1 on the index page

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,44 @@
+var pokemonList=[
+    
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        weight: 6.9,
+        species: 'Seed',
+        types: ['grass', 'poision']
 
+    },
+    {
+        name: 'Butterfree',
+        height: 1.1,
+        weight: 32,
+        species: 'Butterfly',
+        types: ['bug' , 'flying']
 
-alert("Hello World!!!");
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        weight: 6,
+        species: 'Mouse',
+        types: 'Electric'
 
-// let myName="Gitashree Mahato  ";
-// document.write(myName);
+    },
+    {
+        name: 'Jigglypuff',
+        height: 0.5,
+        weight: 5.5,
+        species: 'Balloon',
+        types: ['fairy', 'normal'],
 
+    },
+    {
+        name: 'Snorlax',
+        height: 2.1,
+        weight: 460,
+        species: 'Dozing',
+        types: 'normal'
 
-
+    }
+];
+console.log(pokemonList);
