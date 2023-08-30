94022fba5b1deaacbf36da299a21b73714f59df1
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-08-30 16:44:57 +0200

    update scripts.js
    iterated Pokemon over Pokemonlist using forEach () function.

--- JS/scripts.js
+++ JS/scripts.js
@@ -42,22 +42,104 @@ var pokemonList=[
     }
 ];
 
+let pokemonList2 = [
+    {
+        name: 'Abra',
+        height: 0.9,
+        weight: 19.5,
+        species: 'Telekinesis',
+        types: ['Psychic']
 
-// initialize arraycount for the loop
-const heightThreshold = 2.0;
-// loop through pokemon list
-for (let item = 0; item < pokemonList.length; item++) {
-// printing all pokrmon list from an array
-document.write(` <p> ${pokemonList[item].name} (height: ${pokemonList[item].height}) </p>`);
-// checks wheather height is above the threshold 
-if(pokemonList[item].height > heightThreshold){
+    },
+    {
+        name: 'Nidorina',
+        height: 0.8,
+        weight: 20,
+        species: 'Poison Needle',
+        types: ['Poison']
+    },
+    {
+        name: 'Mankey',
+        height: 0.5,
+        weight: 28,
+        species: 'Pig Monkey',
+        types: ['Fighting']
+    },
+    {
+        name: 'Metapod',
+        height: 0.7,
+        weight: 9.9,
+        species: 'Chrysalis',
+        types: ['Bug']
+    }
+]
+
+
+function getPokemon(pokemon){
+    document.write(`<p> ${pokemon.name}  (species : ${pokemon.species}) </p>`);
+}
+pokemonList.forEach(getPokemon);
+
+
+
+
+
+
+
+
+
+
+
+// pokemonList.forEach(function(pokemon){
+    // document.write(`<p> ${pokemon.name}  (species : ${pokemon.species}) </p>`);
+    // document.write()  
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // initialize arraycount for the loop
+// const heightThreshold = 2.0;
+// // loop through pokemon list
+// for (let item = 0; item < pokemonList.length; item++) {
+// // printing all pokrmon list from an array
+// document.write(` <p> ${pokemonList[item].name} (height: ${pokemonList[item].height}) </p>`);
+// // checks wheather height is above the threshold 
+// if(pokemonList[item].height > heightThreshold){
     
-// printing pokemon list with a statement to identify big pokemon
-document.write(`<p>  ${pokemonList[item].name} (height: ${pokemonList[item].height} ) - <span> Wow, that's big! </span> </p>`);
+// // printing pokemon list with a statement to identify big pokemon
+// document.write(`<p>  ${pokemonList[item].name} (height: ${pokemonList[item].height} ) - <span> Wow, that's big! </span> </p>`);
 
-}
+// }
 
-}
+// }
+
+// function printArrayDetails(list){
+//     for(let i=0; i < list.length; i++){
+//         document.write(`<p> ${list[i].name}   (species : ${list[i].species})`)
+//     }
+// }
+// printArrayDetails(pokemonList)
+// printArrayDetails(pokemonList2)
 
 
 
